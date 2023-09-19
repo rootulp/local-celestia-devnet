87f82f4876c5af989d6cff090a586fe0ed09fa2a
Rootul Patel <rootulp@gmail.com>
2023-09-19 17:02:10 -0400

    debug: identify incompatible architectures
    ```
    starting script...
    which celestia-appd: /bin/celestia-appd
    PATH: /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
    ls -al /bin/celestia-appd: -rwxr-xr-x    1 celestia celestia  85103648
    Sep  6 05:47 /bin/celestia-appd
    file /bin/celestia-appd: /bin/celestia-appd: ELF 64-bit LSB executable,
    ARM aarch64, version 1 (SYSV), dynamically linked, interpreter
    /lib/ld-musl-aarch64.so.1, Go
    BuildID=plbvVXmK4-ywZb4XIWGv/16WCqvuEM8AwNNr3q59t/gRgWRW8IaOiLNxnYJFQj/VBZbxkEqHgZn33Y4fOGI,
    with debug_info, not stripped
    /lib/ld-musl-x86_64.so.1: /bin/celestia-appd: Not a valid dynamic
    program
    ldd /bin/celestia-appd:
    uname -m: x86_64
    arch: x86_64
    /opt/entrypoint.sh: line 17: lscpu: command not found
    lscpu:
    /opt/entrypoint.sh: line 19: /bin/celestia-appd: cannot execute:
    required file not found
    celestia-appd --help:
    ```

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,22 @@
+# The celestia-app version used here should match the version specified in the
+# celestia-node's go.mod.
 FROM ghcr.io/celestiaorg/celestia-app:v1.0.0-rc14 AS celestia-app
-
-FROM ghcr.io/celestiaorg/celestia-node:v0.11.0-rc12
+FROM ghcr.io/celestiaorg/celestia-node:v0.11.0-rc13
 
 USER root
 
 # hadolint ignore=DL3018
 RUN apk --no-cache add \
-        curl \
-        jq \
+    curl \
+    jq \
+    file \
+    linux-headers\
     && mkdir /bridge \
     && chown celestia:celestia /bridge
 
 USER celestia
 
-COPY --from=celestia-app /bin/celestia-appd /bin/
+COPY --chown=celestia --from=celestia-app /bin/celestia-appd /bin/
 
 COPY entrypoint.sh /opt/entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,83 +1,101 @@
 #!/usr/bin/env bash
 
-CHAINID="test"
+# Stop script execution if an error is encountered
+set -o errexit
+# Stop script execution if an undefined variable is used
+set -o nounset
 
-# App & node has a celestia user with home dir /home/celestia
-APP_PATH="/home/celestia/.celestia-app"
-NODE_PATH="/home/celestia/bridge/"
+echo "starting script..."
+FOO=$(which celestia-appd)
+echo "which celestia-appd: ${FOO}"
+echo "PATH: ${PATH}"
+echo "ls -al /bin/celestia-appd: $(ls -al /bin/celestia-appd)"
+echo "file /bin/celestia-appd: $(file /bin/celestia-appd)"
+echo "ldd /bin/celestia-appd: $(ldd /bin/celestia-appd)"
+echo "uname -m: $(uname -m)"
+echo "arch: $(arch)"
+echo "lscpu: $(lscpu)"
 
-# Check if the folder exists
-if [ -d "$APP_PATH" ]; then
-  # If it exists, delete it
-  echo "The folder $APP_PATH exists. Deleting it..."
-  rm -rf "$APP_PATH"
-  echo "Folder deleted."
-else
-  # If it doesn't exist, print a message
-  echo "The folder $APP_PATH does not exist."
-fi
+echo "celestia-appd --help: $(celestia-appd --help)"
 
-# Build genesis file incl account for passed address
-coins="1000000000000000utia"
-celestia-appd init $CHAINID --chain-id $CHAINID
-celestia-appd keys add validator --keyring-backend="test"
-# this won't work because some proto types are declared twice and the logs output to stdout (dependency hell involving iavl)
-celestia-appd add-genesis-account $(celestia-appd keys show validator -a --keyring-backend="test") $coins
-celestia-appd gentx validator 5000000000utia \
-  --keyring-backend="test" \
-  --chain-id $CHAINID
+CELESTIA_APP_HOME="/home/celestia/.celestia-app"
+CELESTIA_APP_VERSION=$(celestia-appd version 2>&1)
 
-celestia-appd collect-gentxs
+echo "celestia-app home: ${CELESTIA_APP_HOME}"
+echo "celestia-app version: ${CELESTIA_APP_VERSION}"
+echo ""
 
-# Set proper defaults and change ports
-# If you encounter: `sed: -I or -i may not be used with stdin` on MacOS you can mitigate by installing gnu-sed
-# https://gist.github.com/andre3k1/e3a1a7133fded5de5a9ee99c87c6fa0d?permalink_comment_id=3082272#gistcomment-3082272
-sed -i'.bak' 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.celestia-app/config/config.toml
-sed -i'.bak' 's/timeout_commit = "25s"/timeout_commit = "1s"/g' ~/.celestia-app/config/config.toml
-sed -i'.bak' 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.celestia-app/config/config.toml
-sed -i'.bak' 's/index_all_keys = false/index_all_keys = true/g' ~/.celestia-app/config/config.toml
-sed -i'.bak' 's/mode = "full"/mode = "validator"/g' ~/.celestia-app/config/config.toml
+CELESTIA_NODE_HOME="/home/celestia/bridge/"
+CELESTIA_NODE_VERSION=$(celestia version)
+echo "celestia-node home: ${CELESTIA_NODE_HOME}"
+echo "celestia-node version: ${CELESTIA_NODE_VERSION}"
+echo ""
 
-# Register the validator EVM address
-{
-  # wait for block 1
-  sleep 20
+# CHAIN_ID="private"
+# KEY_NAME="validator"
+# KEYRING_BACKEND="test"
+# COINS="1000000000000000utia"
+# DELEGATION_AMOUNT="5000000000utia"
 
-  # private key: da6ed55cb2894ac2c9c10209c09de8e8b9d109b910338d5bf3d747a7e1fc9eb9
-  celestia-appd tx qgb register \
-    "$(celestia-appd keys show validator --home "${APP_PATH}" --bech val -a)" \
-    0x966e6f22781EF6a6A82BBB4DB3df8E225DfD9488 \
-    --from validator \
-    --home "${APP_PATH}" \
-    --fees 30000utia -b block \
-    -y
-} &
+# celestia-appd init $CHAIN_ID --chain-id $CHAIN_ID
+# celestia-appd keys add validator --keyring-backend="test"
+# # this won't work because some proto types are declared twice and the logs output to stdout (dependency hell involving iavl)
+# celestia-appd add-genesis-account $(celestia-appd keys show validator -a --keyring-backend="test") $coins
+# celestia-appd gentx validator 5000000000utia \
+#   --keyring-backend="test" \
+#   --chain-id $CHAIN_ID
 
-mkdir -p $NODE_PATH/keys
-cp -r $APP_PATH/keyring-test/ $NODE_PATH/keys/keyring-test/
+# celestia-appd collect-gentxs
 
-# Start the celestia-app
-celestia-appd start &
+# # Set proper defaults and change ports
+# # If you encounter: `sed: -I or -i may not be used with stdin` on MacOS you can mitigate by installing gnu-sed
+# # https://gist.github.com/andre3k1/e3a1a7133fded5de5a9ee99c87c6fa0d?permalink_comment_id=3082272#gistcomment-3082272
+# sed -i'.bak' 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.celestia-app/config/config.toml
+# sed -i'.bak' 's/timeout_commit = "25s"/timeout_commit = "1s"/g' ~/.celestia-app/config/config.toml
+# sed -i'.bak' 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.celestia-app/config/config.toml
+# sed -i'.bak' 's/index_all_keys = false/index_all_keys = true/g' ~/.celestia-app/config/config.toml
+# sed -i'.bak' 's/mode = "full"/mode = "validator"/g' ~/.celestia-app/config/config.toml
 
-# Try to get the genesis hash. Usually first request returns an empty string (port is not open, curl fails), later attempts
-# returns "null" if block was not yet produced.
-GENESIS=
-CNT=0
-MAX=30
-while [ "${#GENESIS}" -le 4 -a $CNT -ne $MAX ]; do
-	GENESIS=$(curl -s http://127.0.0.1:26657/block?height=1 | jq '.result.block_id.hash' | tr -d '"')
-	((CNT++))
-	sleep 1
-done
+# # Register the validator EVM address
+# {
+#   # wait for block 1
+#   sleep 20
 
-export CELESTIA_CUSTOM=test:$GENESIS
-echo $CELESTIA_CUSTOM
+#   # private key: da6ed55cb2894ac2c9c10209c09de8e8b9d109b910338d5bf3d747a7e1fc9eb9
+#   celestia-appd tx qgb register \
+#     "$(celestia-appd keys show validator --home "${CELESTIA_APP_HOME}" --bech val -a)" \
+#     0x966e6f22781EF6a6A82BBB4DB3df8E225DfD9488 \
+#     --from validator \
+#     --home "${CELESTIA_APP_HOME}" \
+#     --fees 30000utia -b block \
+#     -y
+# } &
 
-celestia bridge init --node.store /home/celestia/bridge
-export CELESTIA_NODE_AUTH_TOKEN=$(celestia bridge auth admin --node.store ${NODE_PATH})
-echo "WARNING: Keep this auth token secret **DO NOT** log this auth token outside of development. CELESTIA_NODE_AUTH_TOKEN=$CELESTIA_NODE_AUTH_TOKEN"
-celestia bridge start \
-  --node.store $NODE_PATH --gateway \
-  --core.ip 127.0.0.1 \
-  --keyring.accname validator \
-  --gateway.deprecated-endpoints
+# mkdir -p $CELESTIA_NODE_HOME/keys
+# cp -r $CELESTIA_APP_HOME/keyring-test/ $CELESTIA_NODE_HOME/keys/keyring-test/
+
+# # Start the celestia-app
+# celestia-appd start &
+
+# # Try to get the genesis hash. Usually first request returns an empty string (port is not open, curl fails), later attempts
+# # returns "null" if block was not yet produced.
+# GENESIS=
+# CNT=0
+# MAX=30
+# while [ "${#GENESIS}" -le 4 -a $CNT -ne $MAX ]; do
+# 	GENESIS=$(curl -s http://127.0.0.1:26657/block?height=1 | jq '.result.block_id.hash' | tr -d '"')
+# 	((CNT++))
+# 	sleep 1
+# done
+
+# export CELESTIA_CUSTOM=test:$GENESIS
+# echo $CELESTIA_CUSTOM
+
+# celestia bridge init --node.store /home/celestia/bridge
+# export CELESTIA_NODE_AUTH_TOKEN=$(celestia bridge auth admin --node.store ${CELESTIA_NODE_HOME})
+# echo "WARNING: Keep this auth token secret **DO NOT** log this auth token outside of development. CELESTIA_NODE_AUTH_TOKEN=$CELESTIA_NODE_AUTH_TOKEN"
+# celestia bridge start \
+#   --node.store $CELESTIA_NODE_HOME --gateway \
+#   --core.ip 127.0.0.1 \
+#   --keyring.accname validator \
+#   --gateway.deprecated-endpoints
